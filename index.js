7ca3d20857fd2faf5410d5de5f05e3f96ef4a04e
Aleksandr <aleksandr@gmail.com>
2021-01-16 10:27:19 +0200

    Settinings tabsize

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 'use strict'
 
 class User {
-	constructor(name, surname) {
-		this.name = name;
-		this.surname = surname;
-	}
+   constructor(name, surname) {
+      this.name = name;
+      this.surname = surname;
+   }
 
-	getFullName() {
-		return `${this.name} ${this.surname}`;
-	}
+   getFullName() {
+      return `${this.name} ${this.surname}`;
+   }
 }
 
 class Student extends User {
-	constructor(name, surname, year) {
-		super(name, surname, year);
-		this.year = year;
-	}
+   constructor(name, surname, year) {
+      super(name, surname, year);
+      this.year = year;
+   }
 
-	getCourse() {
-		const thisYear = new Date().getFullYear();
-		const course = thisYear - this.year.getFullYear();
+   getCourse() {
+      const thisYear = new Date().getFullYear();
+      const course = thisYear - this.year.getFullYear();
 
-		if (course > 5 || course < 0) {
-			throw RangeError('You are not a student yet!');
-		}
-		if (+course === 0) {
-			return `Your corse ${1}`;
-		}
-		return `Your corse ${course}`;
-	}
+      if (course > 5 || course < 0) {
+         throw RangeError('You are not a student yet!');
+      }
+      if (+course === 0) {
+         return `Your corse ${1}`;
+      }
+      return `Your corse ${course}`;
+   }
 }
 
 
